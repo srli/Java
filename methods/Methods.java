26deaa755e213d743c1eb68ba4f446465c168d71
srli <sophia.li@students.olin.edu>
2015-12-30 18:46:52 -0500

    print functions are really verbose

---
+++ methods/Methods.java
@@ -0,0 +1,15 @@
+public class Methods{
+	public static void main(String[] args){
+		printOne();
+		printTwo();
+	}
+
+	public static void printOne(){
+		System.out.println("One");
+	}
+
+	public static void printTwo(){
+		System.out.println("Two");
+	}
+
+}

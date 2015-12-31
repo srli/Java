3106e3e63c06c192a176dd29181ab46524231ae2
srli <sophia.li@students.olin.edu>
2015-12-31 13:13:26 -0500

    file io

--- dna_sequencer/Sequencer.java
+++ dna_sequencer/Sequencer.java
@@ -1,8 +1,34 @@
+import java.util.*;
+import java.io.*;
+
 public class Sequencer{
 	public static void main(String[] args){
 		System.out.println("Starting DNA Sequencer!");
+		Map<String, String[]> dict = create_dict();
+		
+		String[] codons = dict.get("F");
+		System.out.println(Arrays.asList(codons).contains("TTT"));
+
 		String t = get_reverse_complement("ATATAT");
 		System.out.println(t);
+	}
+
+	public static Map<String, String[]> create_dict(){
+		Map<String, String[]> map = new HashMap<String, String[]>();
+		try{
+			FileReader input = new FileReader("codons.txt");
+			BufferedReader bufRead = new BufferedReader(input);
+			String myLine = null;
+			while ( (myLine = bufRead.readLine()) != null){    
+			    String aa = myLine.split(" = ")[0];
+			    String[] codons = myLine.split(" = ")[1].split(", ");
+			    map.put(aa, codons);
+			}
+			return map;
+		}
+		catch(Exception e){
+			return null;
+		}
 	}
 
 	public static String get_reverse_complement(String dna){

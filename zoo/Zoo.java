640a56145fc7aaddc9231d68d0a4c787f8d1bf6c
srli <sophia.li@students.olin.edu>
2015-12-31 19:19:50 -0500

    packages and imports are so sucky

---
+++ zoo/Zoo.java
@@ -0,0 +1,11 @@
+package zoo;
+import zoo.animal.Animal;
+
+public class Zoo{
+	public static void println(Object arg){ System.out.println(arg); }
+
+	public static void main(String[] args){
+		Animal puppy = new Animal("zak");
+		println("Hello");
+	}
+}
---
+++ zoo/animal/Animal.java
@@ -0,0 +1,9 @@
+package zoo.animal;
+
+public class Animal{
+	String name;
+
+	public Animal(String in_name){
+		name = in_name;
+	}
+}
